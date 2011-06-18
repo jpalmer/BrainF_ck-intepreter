a77aee8e4b695e7f1175036f6e4e880f62081933
jpalmer <thepalm2@gmail.com>
2011-06-18 23:05:57 +1000

    updated asm - version with while loops now compiles + runs without crashing.  Output is still incorrect

---
+++ compile.sh
@@ -0,0 +1,1 @@
+nasm interp.asm -f win32 -o interp.o -O999 && gcc interp.o -o interp.exe && ./interp.exe
--- interp.asm
+++ interp.asm
@@ -1,14 +1,13 @@
 ;The aim of this is to create a brainfuck intepreter
+;status - working on loop instruction
 extern _printf
 global _main
+
 ;
-; initialized data is put in the .data segment
-;
+; initialized data is put in the .data segment - it may be true that these are not modifiable
 segment .data
-;
-; These labels refer to strings used for output
-;
-program db    "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+++++++++++++++++++++++++++++.+++++++..+++.-------------------------------------------------------------------.------------.+++++++++++++++++++++++++++++++++++++++++++++++++++++++.++++++++++++++++++++++++.+++.------.--------.-------------------------------------------------------------------._", 0       ; don't forget nul terminator
+;program db "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+++++++++++++++++++++++++++++.+++++++..+++.-------------------------------------------------------------------.------------.+++++++++++++++++++++++++++++++++++++++++++++++++++++++.++++++++++++++++++++++++.+++.------.--------.-------------------------------------------------------------------.",0
+program db    ">++++++++++>>>+>+[>>>+[-[<<<<<[+<<<<<]>>[[-]>[<<+>+>-]<[>+<-]<[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>[-]>>>>+>+<<<<<<-[>+<-]]]]]]]]]]]>[<+>-]+>>>>>]<<<<<[<<<<<]>>>>>>>[>>>>>]++[-<<<<<]>>>>>>-]+>>>>>]<[>++<-]<<<<[<[>+<-]<<<<]>>[->[-]++++++[<++++++++>-]>>>>]<<<<<[<[>+>+<<-]>.<<<<<]>.>>>>]", 0       ; don't forget nul terminator
 debug db "dd",0
 intf db "%i" ,10,0
 charf db "%c",0
@@ -21,13 +20,13 @@ output dw "."
 input dw ","
 whilestart dw "["
 whileend dw "]"
-endd dw "_"
 ;
 ; use bss to hold brainfuck program memory
 ;
 segment .bss
 ;memory for program
-array: resw 100
+array: resw 100 ;if you change this need to change in zeromem and printmem routines
+jtab: resb 10000; should support programs up to 10k in length - 10k should be enough for anyone
 datapointer: resw 4 ;not sure why I need to reserve 4 words - reserving only 1 appears to be insufficient
 instrpointer: resw 4
 
@@ -38,10 +37,10 @@ instrpointer: resw 4
 segment .text
 _main:
 	jmp Zeromem
-;Here we zero the memory	
+;Here we zero the memory - this is necersarry because memory may not necersarrily be 0 at the start
 Zeromem:
 	mov ecx, 0 ;0 value
-	mov eax,404
+	mov eax,404 ;404 = memsize *4 +1
 	add eax, array
 	mov ebx, 0
 	add ebx, array
@@ -49,10 +48,66 @@ Zeromem:
 		sub eax,4
 		mov [eax], ecx
 		cmp eax, ebx
-		je initptrs
+		je Buildjmptab
 		jmp .loop_start
 
-;use this to print the memory - mainly useful as a debugging tool + me remembering how to write asm
+;let fixwhiles (program:chars[]) =
+ ;   let startstack = System.Collections.Generic.Stack<_>()
+  ;  let mutable curpointer = -1
+   ; while curpointer+1 < program.Length do
+    ;    curpointer <- curpointer+1
+     ;   match program.[curpointer] with
+      ;  |Whilestart(_) -> startstack.Push(curpointer)
+       ; |Whileend(_) ->
+        ;    let v = startstack.Pop()
+         ;   program.[v] <- Whilestart(curpointer)
+          ;  program.[curpointer] <- Whileend(v)
+        ;|_ -> () //do nothing for other stuff
+    ;program
+Buildjmptab:
+	;Variables I need to store
+		;I use the actual stack as the stack here - why not + frees up registers
+		;ecx - position through program
+		;a,b,d are free for other things - 64bit would be really nice for the extra registers here
+	mov ecx, program
+	dec ecx ;for loop to be easier
+	jmp .loop_start
+	.whilestart
+		push ecx
+		jmp .loop_start
+	.whileend
+		pop eax ;eax is v
+		sub eax, program ;eax is now count through program of original start tax
+		;now I need to calculate the position to put the address
+		lea ebx, [jtab + eax*4] 
+			;explaination - 
+				;edx = start of jump table, 
+				;eax = position through program 
+				;*4 - jump table is ints, program is bytes
+				;program - need to add in base address
+		mov [ebx], ecx; does program.[v] <- curpointer
+		;now do program.[curpointer] <- end(v)
+		mov eax, ecx
+		sub eax, program
+		lea ebx, [jtab + eax*4] ;need to add 4*eax as the jmp table is ints, whilst the program is chars (bytes)
+								;ecx is now the location in the jump table corresponding to the whileend instruction
+		add eax, program
+		mov [ebx], eax; does program.[curpointer] <- v
+		jmp .loop_start
+	.loop_start
+		inc ecx
+		mov al, [ecx]
+		mov dl, [whilestart]
+		cmp dl,al
+		je .whilestart
+		mov dl, [whileend]
+		cmp dl,al
+		je  .whileend
+		mov dl, 0
+		cmp dl,al
+		jne .loop_start
+		jmp initptrs ;now go to main loop
+;use this to print the memory - mainly useful as a debugging tool + me remembering how to write asm - not used currently
 printmem:
 	mov eax,-4
 	add eax, array
@@ -133,7 +188,43 @@ outputC:
 	mov [instrpointer], eax
 	
 	jmp decode
-	
+whilestartC:
+	;check if memory.[pointer] = 0
+	mov eax, [datapointer]
+	cmp eax, 0
+	jne .nojmp
+	;now we need to get the new value for instrpointer
+	mov eax, [instrpointer]
+	lea ebx, [eax*4+jtab]
+	mov ebx, [ebx]
+	inc ebx
+	mov [instrpointer], ebx
+	jmp decode
+	.nojmp 
+		mov eax, [instrpointer]
+		inc eax
+		mov [instrpointer], eax
+		jmp decode
+whileendC:
+	;check if memory.[pointer] = 0
+	mov eax, [datapointer]
+	cmp eax, 0
+	jne .nojmp
+	;now we need to get the new value for instrpointer
+	mov eax, [instrpointer]
+	lea ebx, [eax*4+jtab]
+	mov ebx, [ebx]
+	inc ebx
+	mov [instrpointer], ebx
+	jmp decode
+	.nojmp 
+		mov eax, [instrpointer]
+		inc eax
+		mov [instrpointer], eax
+		jmp decode
+;Instruction decode loop
+;could be made cleverer - but probably safe to assume that all the opcodes are in memory
+;best thing to do could be to map all the input chars to ints 1 - 8 and use those as offsets in a jump table - but this works	
 decode:
 	mov ecx, [instrpointer]
 	mov al, [ecx]
@@ -152,7 +243,13 @@ decode:
 	mov bl, [output]
 	cmp al, bl
 	je outputC
-	mov bl, [endd]
+	mov bl, [whilestart]
+	cmp al, bl
+	je whilestartC
+	mov bl, [whileend]
+	cmp al, bl
+	je whileendC
+	mov bl, 0 ;the input string is nul-terminated, so the final char will be 0 - note that this comparison is not necersarry as the fallthrough goes to the exit case
 	cmp al, bl
 	je exit
 exit:
