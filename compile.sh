b3adf01839d3dc5744f5a6d0715fe8ba5817ffb2
John Palmer <jpal8929@physics.usyd.edu.au>
2011-06-20 10:55:02 +1000

    started linux ver

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #nasm interp.asm -f win32 -o interp.o && gcc interp.o -o interp.exe && ./interp.exe
-nasm interp.asm -f win64 -o interp.o && /usr/bin/x86_64-w64-mingw32-gcc interp.o -o interp.exe && ./interp.exe
+nasm interp.asm -f elf64 -o interp.o && gcc interp.o -o interp.exe && ./interp.exe
